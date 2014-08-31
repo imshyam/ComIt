a1abbf3f65d3e30329cfdb201ba679d8d66ba4df
shyamsundar <sschoudhary0001@gmail.com>
2014-08-31 23:23:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ home = expanduser("~")
 runpyAtHome=home+'/run.py'
 if os.path.isfile(runpyAtHome)==False:
 	os.system("cp run.py ~/")
+	os.system("echo '\nCopied.\n'")
 else:
 	os.system("echo '\nrun.py Already Exists At Home.\n'")
 
