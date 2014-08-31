8f97b6342bb1c71619ecbd558bb4e08a84e8d7f5
iMshyam <sschoudhary0001@gmail.com>
2014-08-31 22:23:53 +0530

    config.py updated

--- config.py
+++ config.py
@@ -1,6 +1,23 @@
 import os
+import os.path
+from os.path import expanduser
 
-f=open('.bashrc','r+')
-f.write("alias comit='python ~/run.py $1 $2'")
-f.close()
-os.system(". ~/.bashrc")
+home = expanduser("~")
+#copy run.py to home folder
+runpyAtHome=home+'/run.py'
+if os.path.isfile(runpyAtHome)==False:
+	os.system("cp ~/Downloads/ComIt/run.py ~/")
+else:
+	os.system("echo '\nrun.py Already Exists At Home.\n'")
+
+
+filename=home+'/.bashrc'
+#checking if command already exists
+if 'comit' in open(filename).read():
+	os.system("echo '\nCommand \"comit\" Already Exists.\n'")
+#else Create a command
+else:
+	f=open(filename,'a')
+	f.write("\nalias comit='python ~/run.py $1 $2'")
+	f.close()
+	os.system(". ~/.bashrc")
