e6852ad9cd13bf2d8d18bfd60b7a896e3af0f1be
iMshyam <sschoudhary0001@gmail.com>
2014-08-31 16:13:00 +0530

    languages added

---
+++ examplecc.cc
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout<<"output is this."<<endl;
+}
--- run.py
+++ run.py
@@ -10,22 +10,43 @@ CLIENT_SECRET = '807834b1529d46b54b22f7427156bf95562db4df'
 filename=sys.argv[1]
 source = open(filename, 'r')
 language=filename.rsplit('.',1)[1]
+#supported languages:
 #C C++ Clojure Haskell C# Java Objective-C Perl PHP Python ruby
-print "\nLanguage Detected : ",language.upper(),"\n"
-if language.upper()=="C":
+#C, CPP, CPP11, CLOJURE, CSHARP, JAVA, JAVASCRIPT, HASKELL, PERL, PHP, PYTHON, RUBY
+
+if language.upper()=="C"  or language.upper()=="H":#.c .h
 	lang="C"
-elif language.upper()=="CPP":
+	print "\nLanguage Detected : ",lang,"\n"
+elif language.upper()=="CPP" or language.upper()=="CC" or language.upper()=="CXX" or language.upper()=="C++" or language.upper()=="H" or language.upper()=="HH":#.cc .cpp .cxx .c++ .h .hh 
 	lang="CPP"
-elif language.upper()=="PY":
+	print "\nLanguage Detected : ",lang "or CPP11","\n"
+elif language.upper()=="PY" or language.upper()=="PYW" or language.upper()=="PYC" or language.upper()=="PYO" or language.upper()=="PYD":#.py .pyw, .pyc, .pyo, .pyd
 	lang="PYTHON"
-elif language.upper()=="CS":
-	lang="CS"
-elif language.upper()=="CD":
-	lang="CS"
-elif language.upper()=="CDF":
-	lang="CDf"
+	print "\nLanguage Detected : ",lang,"\n"
+elif language.upper()=="CLJ" or language.upper()=="EDN":#.clj .edn
+	lang="CLOJURE"
+	print "\nLanguage Detected : ",lang,"\n"
+elif language.upper()=="JAVA" language.upper()=="CLASS" or language.upper()=="JAR":#.java .class .jar
+	lang="JAVA"
+	print "\nLanguage Detected : ",lang,"\n"
+elif language.upper()=="RB" or language.upper()=="RBW":#.rb .rbw
+	lang="RUBY"
+	print "\nLanguage Detected : ",lang,"\n"#.js
+elif language.upper()=="JS":
+	lang="JAVASCRIPT"
+	print "\nLanguage Detected : ",lang,"\n"
+elif language.upper()=="HS" or language.upper()=="LHS":#.hl .hls
+	lang="HASKELL"
+	print "\nLanguage Detected : ",lang,"\n"
+elif language.upper()=="PL" or language.upper()=="PM" or language.upper()=="T" or language.upper()=="POD" or language.upper()=="LHS":#.pl .pm .t .pod
+	lang="PERL"
+	print "\nLanguage Detected : ",lang,"\n"
+elif language.upper()=="PHP" or language.upper()=="PHTML" or language.upper()=="PHP4" or language.upper()=="PHP3" or language.upper()=="PHP5" or language.upper()=="PHPS":#.php, .phtml, .php4, .php3, .php5, .phps
+	lang="PHP"
+	print "\nLanguage Detected : ",lang,"\n"
 else:
 	print "This Is Not A Compitible Language.\n"
+	print "Extention Detected : ",language.upper(),"\n"
 	sys.exit()
 
 data = {
